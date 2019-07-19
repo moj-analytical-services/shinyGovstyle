cf165c97941bf462791acd7a3d4be4614e0c44e9
rosswyatt <ross.wyatt@digital.justice.gov.uk>
2019-07-19 10:56:25 +0100

    add small checkbox class

--- R/checkbox_input.R
+++ R/checkbox_input.R
@@ -6,17 +6,24 @@
 #' @param cb_values Add the values that will create the div IDs in the form of paste(cb_,cb_values)
 #' @param text_input Insert the text for the checkbox group.
 #' @param hint_text Insert optional hint/secondary text.  Defaults to NULL
+#' @param small change the sizing to a small version of the checkbox.  Defaults to \code{FALSE}
 #' @keywords checkbox
 #' @export
 #' @examples
 #' checkbox_input(c("Option 1", "Option 2", "Option 3"), c("op1", "op2", "op3"), "Choice option", "Lets go nuts")
 
-checkbox_input <- function(cb_labels, inputIds, text_input, hint_input = NULL){
+checkbox_input <- function(cb_labels, inputIds, text_input, hint_input = NULL, small = FALSE){
+  if (small){
+    class_build <- "govuk-checkboxes govuk-checkboxes--small"
+  } else {
+    class_build <- "govuk-checkboxes"
+  }
+
   tags$div(class="govuk-form-group shiny-input-checkboxgroup", id = "test",
     tags$fieldset(class="govuk-fieldset",
       tags$label(text_input, class="govuk-label"),
       tags$span(hint_input, class="govuk-hint"),
-      tags$div(class="govuk-checkboxes",
+      tags$div(class=class_build,
         Map(function(x, y) {
           value <- restoreInput(id = y, default = FALSE)
           tags$div(class="govuk-checkboxes__item", id=paste0("div_", y),
