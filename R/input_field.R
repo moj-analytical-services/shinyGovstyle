8f9a3ffa31f6af60c627d6e15ce909c884c4b445
rosswyatt <ross.wyatt@digital.justice.gov.uk>
2020-03-10 18:07:13 +0000

    input field fix

--- R/input_field.R
+++ R/input_field.R
@@ -6,16 +6,56 @@
 #' @param inputIds A list input slots that will be used to access the value.
 #' @param widths control the size of the box based on number of characters required.  Options are 30, 20, 10, 5, 4, 3, 2.  NULL will not limit the size
 #' @param types text box types.  Will default to text.
+#' @param error Whenever to icnlud error handling  Defaults to FALSE.
+#' @param error_message Message to display on error.  Defaults to NULL
 #' @keywords input field
 #' @export
 #' @examples
-#' input_field("List of three text boxes", c("Option 1", "Option 2", "Option 3"), c("op1", "op2", "op3"))
+#' if (interactive()) {
+#'
+#'   ui <- fluidPage(
+#'     # Required for error handling function
+#'     shinyjs::useShinyjs(),
+#'     shinyGovstyle::header(
+#'       main_text = "Example",
+#'       secondary_text = "User Examples",
+#'       logo="shinyGovstyle/images/moj_logo.png"),
+#'     shinyGovstyle::banner(inputId = "banner", type = "beta", 'This is a new service'),
+#'     shinyGovstyle::gov_layout(size = "two-thirds",
+#'       shinyGovstyle::input_field(
+#'         legend ="List of three text boxes in a field",
+#'         labels = c("Field 1", "Field 2", "Field 3"),
+#'         inputIds = c("field1", "field2", "field3"),
+#'         widths = c(30,20,10),
+#'         error = TRUE),
+#'      # Button to trigger error
+#'      shinyGovstyle::button_Input(inputId = "submit", label = "Submit")
+#'     ),
+#'     shinyGovstyle::footer(full = TRUE)
+#'   )
+#'
+#'   server <- function(input, output, session) {
+#'     # Trigger error on blank submit of field2
+#'     observeEvent(input$submit, {
+#'       if (input$field2 == ""){
+#'         shinyGovstyle::error_on(inputId = "field2", error_message = "Please complete")
+#'       } else {
+#'         shinyGovstyle::error_off(
+#'           inputId = "field2")
+#'       }
+#'     })
+#'   }
+#'   shinyApp(ui = ui, server = server)
+#' }
 
 input_field <- function(legend, labels, inputIds, widths=NULL, types = "text", error = FALSE, error_message = NULL){
+  if (is.null(widths)){
+    widths <- rep_len(0,length(inputIds))
+  }
   govInputField <- tags$fieldset(class="govuk-fieldset",
     tags$legend(legend, class="govuk-fieldset__legend govuk-fieldset__legend--xl"),
     Map(function(x, y, z, a){
-      if (is.null(z)){
+      if (z == 0){
         width_class <- "govuk-input"
       }
       else{
