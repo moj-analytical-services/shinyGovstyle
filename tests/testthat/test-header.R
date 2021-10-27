aa1f2cc0b60796d097206a86cde6e8d98015fd94
rosswyatt <ross.wyatt@digital.justice.gov.uk>
2021-10-27 13:56:31 +0100

    Add crown logo to the header

--- tests/testthat/test-header.R
+++ tests/testthat/test-header.R
@@ -2,7 +2,7 @@ test_that("multiplication works", {
   header_check <- header("Test", "Again")
 
   expect_identical(
-    header_check$children[[1]]$attribs$class,
+    header_check$children[[2]]$attribs$class,
     "govuk-header__container govuk-width-container"
   )
 })
