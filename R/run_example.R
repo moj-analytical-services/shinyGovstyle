14ff346c84e4fb97eb2169dfc561023d1fb60a57
rosswyatt <ross.wyatt@digital.justice.gov.uk>
2021-10-27 20:03:00 +0100

    rewrite example

--- R/run_example.R
+++ R/run_example.R
@@ -11,112 +11,177 @@
 run_example <- function(){
   shiny::shinyApp(
     ui = shiny::fluidPage(
+      title="ShinyGovstyle",
+      header("MOJ", "ShinyGovstyle Example",
+             logo="shinyGovstyle/images/moj_logo.png", logo_height = 38),
+      banner(
+        "banner",
+        "beta",
+        'This is a new service \u002D your <a class="govuk-link" href="#">
+        feedback</a> will help us to improve it.'),
+
+      #Need this to make the error and word count work
       shinyjs::useShinyjs(),
-      #font(),
-      shinyGovstyle::header("Justice",
-                            "Prototype",
-                            logo="shinyGovstyle/images/moj_logo.png"),
-      shinyGovstyle::gov_layout(size = "full",
 
-        shinyGovstyle::backlink_Input("backid"),
+      #Add a message to show users how to see this code
+      shiny::tags$br(),
+      shiny::markdown(
+        "Run ```View(run_example)``` in console to see the code for this app"),
 
-        shinyGovstyle::radio_button_Input(
-          inputId = "input1",
-          choices  = c("Red", "Amber", "Green"),
-          label = "Delivery RAG rating"),
+      #Set up a nav panel so everything not on single page
+      shiny::navlistPanel(
+        "",
+        id="nav",
+        widths = c(2, 10),
+        well = FALSE,
 
-        shinyGovstyle::checkbox_Input(
-          inputId = "checkID",
-          cb_labels = c(
-            "Waste from animal carcasses",
-            "Waste from mines or quarries",
-            "Farm or agricultural waste"),
-          checkboxIds = c("op1", "op2", "op3"),
-          label = "Which types of waste do you transport?",
-          hint_label = "Select all that apply."),
+        #Create first panel
+        shiny::tabPanel(
+          "Select Types",
+          value = "panel1",
+          gov_layout(
+            size = "two-thirds",
+            heading_text("Page 1", size = "l"),
+            label_hint("label1", "These are some examples of the types of user
+                   select type inputs that you can use"),
+            heading_text("radio_button_Input", size = "s"),
+            radio_button_Input(
+              inputId = "name_changed", label = "Have you changed your name?",
+              choices = c("Yes", "No"), inline = TRUE,
+              hint_label = "This includes changing your last name or spelling
+                            your name differently."),
+            heading_text("checkbox_Input", size = "s"),
+            checkbox_Input(
+              inputId = "checkID",
+              cb_labels = c("Waste from animal carcasses",
+                            "Waste from mines or quarries",
+                            "Farm or agricultural waste"),
+              checkboxIds = c("op1", "op2", "op3"),
+              label = "Which types of waste do you transport?",
+              hint_label = "Select all that apply."),
+            heading_text("select_Input", size = "s"),
+            select_Input(
+              inputId = "sorter",
+              label = "Sort by",
+              select_text = c("Recently published",
+                              "Recently updated",
+                              "Most views",
+                              "Most comments"),
+              select_value = c("published", "updated", "view", "comments")),
+            heading_text("file_Input", size = "s"),
+            file_Input(inputId = "file1", label = "Upload a file"),
+            heading_text("button_Input", size = "s"),
+            button_Input("btn1", "Go to next page")
+          )
+        ),
 
-        shinyGovstyle::button_Input(inputId = "btn1", label = "default"),
 
-        shinyGovstyle::button_Input(inputId = "btn2",
-                                    label = "start",
-                                    type = "start"),
+        #Create second panel
+        shiny::tabPanel(
+          "Text Types",
+          value = "panel2",
+          gov_layout(
+            size = "two-thirds",
+            heading_text("Page 2", size = "l"),
+            label_hint("label2", "These are some examples of the types of user
+                   text inputs that you can use"),
+            heading_text("date_Input", size = "s"),
+            date_Input(
+              inputId = "date1",
+              label = "What is your date of birth?",
+              hint_label = "For example, 31 3 1980"),
+            heading_text("text_Input", size = "s"),
+            text_Input(inputId = "txt1", label = "Event name"),
+            heading_text("text_area_Input", size = "s"),
+            text_area_Input(
+              inputId = "text_area1",
+              label = "Can you provide more detail?",
+              hint_label = "Do not include personal or financial information,
+          like your National Insurance number or credit card details."),
+            text_area_Input(
+              inputId = "text_area2",
+              label = "How are you today?",
+              hint_label = "Leave blank to trigger error",
+              error = T,
+              error_message = "Please do not leave blank",
+              word_limit = 300),
+            heading_text("button_Input", size = "s"),
+            button_Input("btn2", "Go to next page"),
+            button_Input("btn3", "Check for errors", type = "warning")
+          )
+        ),
 
-        shinyGovstyle::button_Input(inputId = "btn3",
-                                    label = "secondary",
-                                    type = "secondary"),
+        #Create third panel
+        shiny::tabPanel(
+          "Feedback Types",
+          value = "panel3",
+          gov_layout(
+            size = "two-thirds",
+            heading_text("Page 3", size = "l"),
+            label_hint("label3", "These are some examples of the types of user
+                   feedback inputs that you can use"),
+            heading_text("details", size = "s"),
+            details(
+              inputId = "detID",
+              label = "Help with nationality",
+              help_text = "We need to know your nationality so we can work out
+              which elections you\u0027re entitled to vote in. If you cannot provide
+              your nationality\u002C you\u0027ll have to send copies of identity
+              documents through the post."),
+            heading_text("insert_text", size = "s"),
+            insert_text(inputId = "insertId",
+                        text = "It can take up to 8 weeks to register a lasting
+                        power of attorney if there are no mistakes in the
+                        application."),
+            heading_text("warning_text", size = "s"),
+            warning_text(
+              inputId = "warn",
+              text = "You can be fined up to \u00A35\u002C000 if you do
+              not register."),
+            heading_text("panel_output", size = "s"),
+            panel_output(
+              inputId = "panId",
+              main_text = "Application complete",
+              sub_text = "Your reference number <br> <strong>HDJ2123F</strong>"),
+            heading_text("noti_banner", size = "s"),
+            noti_banner(
+              "notId",
+              title_txt = "Important",
+              body_txt = "You have 7 days left to send your application.",
+              type = "standard"
+            )
+          )
+        )),
 
-        shinyGovstyle::button_Input(inputId = "btn4",
-                                    label = "warning",
-                                    type = "warning"),
-
-        shinyGovstyle::select_Input(
-          inputId = "sorter",
-          label = "Sort by",
-          select_text = c(
-            "Recently published",
-            "Recently updated",
-            "Most views",
-            "Most comments"),
-          select_value = c("published", "updated", "view", "comments")),
-
-        shiny::tags$br(),
-
-        shinyGovstyle::date_Input(
-          inputId = "date1",
-          label = "What is your date of birth?",
-          hint_label = "For example, 31 3 1980",
-          day = 16, month = 12, year = 1983),
-
-        shinyGovstyle::file_Input(inputId = "file1", label = "Upload a file"),
-
-        shinyGovstyle::text_Input(inputId = "txt1", label = "Event name",
-                                  prefix = "boo", suffix = "Woo", error = TRUE),
-
-        shinyGovstyle::text_area_Input(
-          inputId = "text_area",
-          label = "Can you provide more detail?",
-          hint_label = "Do not include personal or financial information, like
-                        your National Insurance number or credit card details.",
-          error = TRUE, word_limit = 300),
-
-        shinyGovstyle::warning_text(
-          inputId = "warn",
-          text = "You can be fined up to \u00A35000 if you do not register."),
-
-        shinyGovstyle::insert_text(
-          inputId = "insertId",
-          text = "It can take up to 8 weeks to register a lasting power of
-                  attorney if there are no mistakes in the application."),
-
-        shinyGovstyle::details(
-          inputId = "detID",
-          label = "Help with nationality",
-          help_text = "We need to know your nationality so we can work out
-                       which elections you\u0027re entitled to vote in. If you
-                       cannot provide your nationality\u002C you\u0027ll have to
-                       send copies of identity documents through the post."),
-
-        shinyGovstyle::panel_output(
-          inputId = "panId",
-          main_text = "Application complete",
-          sub_text = "Your reference number <br> <strong>HDJ2123F</strong>")
-      ),
-      shinyGovstyle::footer(TRUE)
-  ),
+      # Add a full footer
+      footer(TRUE)),
 
   server = function(input, output, session) {
-    err <- shiny::reactiveVal(FALSE)
-    shiny::observeEvent(input$btn1,{
-      if (err()) {
-        err(FALSE)
-        error_off("txt1")
-        error_off("text_area")
-      } else {
-        err(TRUE)
-        error_on("txt1", "You have an error")
-        error_on("text_area", "You have an error")
-      }
-  })
+    # Nav to next tab
+    shiny::observeEvent(input$btn1, {
+      shiny::updateTabsetPanel(session, "nav",
+                        selected = "panel2")
+    })
 
+    # Nav to next tab
+    shiny::observeEvent(input$btn2, {
+      shiny::updateTabsetPanel(session, "nav",
+                        selected = "panel3")
+    })
+
+    # Need this to use live update the word counter
+    shiny::observeEvent(
+      input$text_area2,
+      word_count("text_area2", input$text_area2)
+    )
+
+    # Trigger error if text_are2 is blank
+    shiny::observeEvent(input$btn3, {
+      if (input$text_area2 == "") {
+        error_on("text_area2")
+      } else {
+        error_off("text_area2")
+      }
+    })
   }
 )}
