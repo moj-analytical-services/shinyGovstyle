d56157446b74e45fa0a0ef0cfe2b42b22871a9e0
rosswyatt <ross.wyatt@digital.justice.gov.uk>
2021-10-28 10:21:00 +0100

    update back link to a button

--- R/backlink_Input.R
+++ R/backlink_Input.R
@@ -2,14 +2,54 @@
 #'
 #' This function adds a back link to the page
 #' @param inputId The input slot that will be used to access the value.
-#' @param link Link for when the button is clicked  Defaults to #
 #' @return a backlink html shiny object
 #' @keywords backlink
 #' @export
 #' @examples
-#' backlink_Input("backid")
+#' if (interactive()) {
+#'   ui <- fluidPage(
+#'     header(
+#'       main_text = "Example",
+#'       secondary_text = "User Examples",
+#'       logo="shinyGovstyle/images/moj_logo.png"),
+#'     shiny::navlistPanel(
+#'       "",
+#'       id="nav",
+#'       widths = c(2, 10),
+#'       well = FALSE,
+#'
+#'       #Create first panel
+#'       shiny::tabPanel(
+#'         "Select Types",
+#'         value = "panel1",
+#'         gov_layout(size = "two-thirds",
+#'           backlink_Input("link1"),
+#'           shiny::tags$br(), shiny::tags$br()
+#'        )),
+#'        shiny::tabPanel(
+#'          "Tab2",
+#'          value = "panel2")),
+#'    shinyGovstyle::footer(full = TRUE)
+#'   )
+#'
+#'   server <- function(input, output, session) {
+#'     #Slightly confused in that it goes forward rather than back, but shows how
+#'     #to use
+#'     observeEvent(input$link1,{
+#'       updateTabsetPanel(session, "nav", selected = "panel2")
+#'     })
+#'   }
+#'   shinyApp(ui = ui, server = server)
+#' }
 
-backlink_Input <- function(inputId, link = "#") {
-  govBacklink <- shiny::tags$a("Back", href=link, class="govuk-back-link")
+backlink_Input <- function(inputId) {
+
+  value <- shiny::restoreInput(id = inputId, default = NULL)
+  govBacklink <- shiny::tags$button("Back",
+                   id = inputId,
+                   class = paste0("govuk-back-link",
+                                  " action-button"),
+                   `data-val` = value)
   attachDependency(govBacklink)
+
 }
