64c4210667dace1723d5f0c9b5316d4340414ff4
rosswyatt <ross.wyatt@digital.justice.gov.uk>
2021-11-11 21:55:19 +0000

    make summary a function

--- R/summary.R
+++ R/summary.R
@@ -1,10 +1,47 @@
 
-headers <- c("Name", "Date of birth", "Contact information", "Contact details")
-info <- c("Sarah Philips",
-          "5 January 1978", "72 Guild Street <br> London <br> SE23 6FH",
-          "07700 900457 <br> sarah.phillips@example.com")
+#' Tabs Function
+#'
+#' This function creates a tabs based table.  It requires a single dataframe
+#' with a grouping variable
+#' @param inputId The id to access the summary list
+#' @param headers input for the row headers value
+#' @param info summary information values for the table
+#' @param action whenever a change link is needed.  sets input to the value of
+#' the headers using lowercase and with underscore to replace gaps. Default
+#' set to \code{FALSE}
+#' @param border set if the table should have borders.  Default set
+#' to \code{TRUE}
+#' @return a summary list table html shiny object.
+#' @keywords summary list
+#' @export
+#' @examples
+#' if (interactive()) {
+#'
+#'   # Create an example dataset
+#'   headers <- c("Name", "Date of birth", "Contact information", "Contact details")
+#'   info <- c(
+#'     "Sarah Philips",
+#'     "5 January 1978",
+#'     "72 Guild Street <br> London <br> SE23 6FH",
+#'     "07700 900457 <br> sarah.phillips@example.com")
+#'
+#'   ui <- fluidPage(
+#'     shinyGovstyle::header(
+#'       main_text = "Example",
+#'       secondary_text = "User Examples",
+#'       logo="shinyGovstyle/images/moj_logo.png"),
+#'     shinyGovstyle::gov_layout(size = "two-thirds",
+#'       shinyGovstyle::gov_summary(headers, info, action = FALSE)),
+#'     shinyGovstyle::footer(full = TRUE)
+#'   )
+#'
+#'   server <- function(input, output, session) {}
+#'   shinyApp(ui = ui, server = server)
+#' }
+#'
 
-gov_summary <- function(headers, info, action = FALSE, border = TRUE){
+
+gov_summary <- function(inputId, headers, info, action = FALSE, border = TRUE){
 
   if (border){
     border_class = "govuk-summary-list"
@@ -14,6 +51,7 @@ gov_summary <- function(headers, info, action = FALSE, border = TRUE){
 
   shiny::tags$dl(
     class = border_class,
+    id = inputId,
     Map(function(x, y, z){
       shiny::tags$div(
         class = "govuk-summary-list__row",
