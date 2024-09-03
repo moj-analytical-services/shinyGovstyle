3f15e2b56fb2b8452b76c08f9944f69235fab298
jenisayshi <166698885+jenisayshi@users.noreply.github.com>
2024-09-03 10:19:48 +0100

    add onclick to contents link to scroll to top of window

--- R/contents_links.R
+++ R/contents_links.R
@@ -162,6 +162,7 @@ contents_links <- function(link_text,
   contents_div <- shiny::tags$div(
     class = "govuk-contents",
     shiny::actionLink(
+      onclick = "window.scrollTo(0, 0);",
       class = "govuk-contents__link",
       inputId = input_id,
       label = link_text
