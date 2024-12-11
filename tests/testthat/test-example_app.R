a633382e4e80f9c3f220726d565b6ec6ffac0c06
Cam Race <52536248+cjrace@users.noreply.github.com>
2024-12-11 18:16:15 +0000

    move example app to own directory

--- tests/testthat/test-example_app.R
+++ tests/testthat/test-example_app.R
@@ -4,6 +4,9 @@ test_that("App loads and title of app appears as expected", {
   # Don't run these tests on the CRAN build servers
   skip_on_cran()
 
+  appdir <- system.file(package = "shinyGovstyle", "example_app")
+  test_app(appdir)
+
   shiny_app <- run_example()
   app <- AppDriver$new(shiny_app, name = "showcase")
 
