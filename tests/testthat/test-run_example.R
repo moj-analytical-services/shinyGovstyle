f9520f8afc5d238544b3285bec0aea497e55fb0b
Cam Race <52536248+cjrace@users.noreply.github.com>
2024-12-11 17:28:08 +0000

    add shinytest2 for testing run example and draft yaml for deploy (#118)
    * add shinytest2 for testing run example and draft yaml for deploy
    
    * update link / guidance for showcase app code
    
    * add master to branches for deploy
    
    * tidy up next page buttons and relabel id for error button

--- tests/testthat/test-run_example.R
+++ tests/testthat/test-run_example.R
@@ -1,12 +1,14 @@
+library(shinytest2)
 
-test_that("shiny example loads", {
-  shiny::testServer(
-    app = run_example(), {
-    expect_equal(
-      session$isClosed(),
-      FALSE
-    )
+test_that("App loads and title of app appears as expected", {
+  # Don't run these tests on the CRAN build servers
+  skip_on_cran()
 
-    }
+  shiny_app <- run_example()
+  app <- AppDriver$new(shiny_app, name = "showcase")
+
+  expect_equal(
+    app$get_text("title"),
+    "ShinyGovstyle"
   )
 })
