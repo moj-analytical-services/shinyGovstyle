b7e897659489dad2a6d8735ffde0ab7cc2821ea3
Cam Race <52536248+cjrace@users.noreply.github.com>
2024-12-11 15:37:05 +0000

    fix xlsx - xslx typo in bad link text

--- data-raw/bad_link_text.R
+++ data-raw/bad_link_text.R
@@ -4,12 +4,12 @@ bad_link_text <- data.frame(
     "click", "csv", "continue", "dashboard", "document", "download", "file",
     "form", "guidance", "here", "info", "information", "jpeg", "jpg", "learn",
     "link", "more", "next", "page", "pdf", "previous", "read", "site", "svg",
-    "this", "web", "webpage", "website", "word", "xslx",
+    "this", "web", "webpage", "website", "word", "xlsx",
     # two word examples
     "click here", "click this link", "download csv", "download document",
     "download file", "download here", "download jpg", "download jpeg",
     "download pdf", "download png", "download svg", "download word",
-    "download xslx", "further information", "go here", "learn more",
+    "download xlsx", "further information", "go here", "learn more",
     "link to", "read more", "this page", "visit this", "web page", "web site",
     "this link"
   )
